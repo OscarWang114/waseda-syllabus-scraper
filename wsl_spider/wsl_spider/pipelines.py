4e937fd470062864a9854ba2dad6224f7f0cccd4
OscarWang114 <chihhaowang.oscar@gmail.com>
2017-10-10 06:52:13 +0800

    Create FirstPagePipeline for saving html files locally

--- wsl_spider/wsl_spider/pipelines.py
+++ wsl_spider/wsl_spider/pipelines.py
@@ -4,8 +4,19 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import os
+
+save_path = os.path.dirname(os.path.abspath(__file__))
+
+print(save_path)
+
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
 
 
-class WslSpiderPipeline(object):
+class FirstPagePipeline(object):
     def process_item(self, item, spider):
+        filename = item['name'] + ".html"
+        with open(os.path.join(save_path, filename), 'wb') as f:
+            f.write(item['html'])
         return item
